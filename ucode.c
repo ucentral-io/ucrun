ucentral-io/ucrun

commit 5f3e47496d784ba653b73ef8ff38a14e6d90f3f2

ucode: initialize default library search path
Recent ucode versions require us to explicitly initialize the search path
structure in the parser config.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

ucode.c
@@ -385,6 +385,7 @@ ucode_init(ucrun_ctx_t *ucrun, int argc, const char **argv, int *rc)
 	/* initialize VM context */
 	uc_vm_init(&ucrun->vm, &config);
 	uc_vm_exception_handler_set(&ucrun->vm, ucode_handle_exception);
+	uc_search_path_init(&config.module_search_path);
 
 	/* load our user code */
 	ucrun->prog = ucode_load(argv[1]);

commit 5be6abebc4ae6057b47a5b3f0799d5ff01bc60c3

ucode: initialize search path before VM init
Since the VM initialization copies the search path into a runtime array,
we must prepare the path beforehand.

Fixes: 5f3e474 ("ucode: initialize default library search path")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

ucode.c
@@ -383,9 +383,9 @@ ucode_init(ucrun_ctx_t *ucrun, int argc, const char **argv, int *rc)
 	INIT_LIST_HEAD(&ucrun->process);
 
 	/* initialize VM context */
+	uc_search_path_init(&config.module_search_path);
 	uc_vm_init(&ucrun->vm, &config);
 	uc_vm_exception_handler_set(&ucrun->vm, ucode_handle_exception);
-	uc_search_path_init(&config.module_search_path);
 
 	/* load our user code */
 	ucrun->prog = ucode_load(argv[1]);